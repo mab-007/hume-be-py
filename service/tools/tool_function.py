9b68357b66acb5f1da9d1436c84ef175612db75b
pranav <pranav@GL-LAP-BLR-0076.local>
2025-02-23 16:58:25 +0530

    location topics

--- service/tools/tool_function.py
+++ service/tools/tool_function.py
@@ -24,3 +24,13 @@ class ToolFunction:
         except Exception as e:
             print(f"Error grounding google: {e}")
             return "Something went wrong!"
+    
+    async def location_topics(self, body):
+        try:
+            print(f"body: {body}")
+            location = body.get("location")
+            system_prompt = "You will be given a location anywhere from the world. Your job is to find conversational topics relevant to the location that a person who has grown up there or stays there will find relevant. This can include things like what the city is famous for, famous spots, weather etc. The topics you find should be relevant for a person who has grown up in the city, a localite."
+            return self.gemini_client.grounding_google(prompt=location, model='gemini-2.0-pro',system_prompt=system_prompt)
+        except Exception as e:
+            print(f"Error grounding google: {e}")
+            return "Something went wrong!"
